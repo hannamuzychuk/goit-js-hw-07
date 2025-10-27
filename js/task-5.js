7fec82d7cdc864291a09a594271807abb0120b62
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-10-27 13:17:05 +0100

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
