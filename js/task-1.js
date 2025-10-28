aefce8fc384e8f6b0bb63d4c60949526f108c15e
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-10-28 09:16:27 +0100

    add change task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,3 @@
-// const categoriesItem = document.querySelectorAll("ul#categories  li.item");
-// const numberOfCategories = categoriesItem.length;
-// console.log('Number of categories:', numberOfCategories);
-
-// categoriesItem.forEach(item => {
-//     const title = item.querySelector("h2").textContent;
-//     const numberOfItems = item.querySelectorAll("ul li").length;
-//     console.log(`Category: ${title}`);
-//     console.log(`Elements: ${numberOfItems}`);
-// });
-
 const categoryItem = document.querySelectorAll("ul#categories li.item");
 const countCategoryItem = categoryItem.length;
 console.log(`Number of categories: ${countCategoryItem}`);
